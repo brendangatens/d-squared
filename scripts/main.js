ac2867cc2261082f85ff6e20fa379b80228dcfce
Brendan Gatens <howdy@brendangatens.com>
2014-03-04 13:20:09 +0000

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+  /* $("body").css( "padding-top", "30px" ); */
+  $("body").prepend( '<div class="notice"><a href="http://claregerrard.com/">claregerrard.com</a></div>' );
+});
